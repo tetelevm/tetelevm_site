e3bf0818678a2c12cf16188352097607c789550a
tetelevm <tetelevm@proton.me>
2026-08-27 08:20:57 +0300

    Add gsv-token

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,24 +1,29 @@
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 
-const caddyPageMeta = {
-  name: "caddy-page-meta",
+const caddyTemplates = {
+  name: "caddy-templates",
   apply: "build",
   transformIndexHtml(html) {
-    return html.replace(
-      "<!-- page-meta -->",
-      `{{if eq (placeholder "http.error.status_code") "404"}}
+    return html
+      .replace(
+        "<!-- google-site-verification -->",
+        `<meta name="google-site-verification" content='{{placeholder "env.GOOGLE_SITE_VERIFICATION"}}' />`,
+      )
+      .replace(
+        "<!-- page-meta -->",
+        `{{if eq (placeholder "http.error.status_code") "404"}}
       <title data-page-meta>Страница не найдена</title>
       <meta name="robots" content="noindex" data-page-meta>
       {{else}}
       {{httpInclude (printf "/_api/page-meta/?path=%s" .OriginalReq.URL.Path)}}
       {{end}}`,
-    )
+      )
   },
 }
 
 export default defineConfig({
-  plugins: [vue(), caddyPageMeta],
+  plugins: [vue(), caddyTemplates],
   server: {
     proxy: {
       "/_api": {
